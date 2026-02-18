601f660eb345aa75f1c2a06d10fba5c84aa31974
7x <info@se7enx.com>
2026-02-18 06:20:04 -0800

    Added: Added refactored base pacgelayout implementation extension to provide for customizations to better provide a polished ui look for ng admin ui bundle admin dashboard. New feature is refined adminui look changes. Enhancement.

---
+++ bundle/Version.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Netgen\TagsBundle;
+
+/**
+ * Version class for Netgen Tags Bundle
+ */
+class Version
+{
+    const MAJOR_VERSION = 3;
+    const MINOR_VERSION = 0;
+    const RELEASE_VERSION = 0;
+    const EXTRA_VERSION = '';
+
+    const RELEASE = self::MAJOR_VERSION . '.' . self::MINOR_VERSION . '.' . self::RELEASE_VERSION . self::EXTRA_VERSION;
+
+    public static function VERSION()
+    {
+        return static::RELEASE;
+    }
+
+    public static function apiVersion()
+    {
+        return static::RELEASE;
+    }
+}
